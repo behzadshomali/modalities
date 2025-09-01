f0cba0e35174d0c8590cebd551ae94f20d24abc8
Behzad Shomali <behzad.shomali@gmail.com>
2025-09-01 10:23:37 +0200

    feat: make merge_lora file easier to use

--- src/modalities/instruction_finetuning/merge_lora.py
+++ src/modalities/instruction_finetuning/merge_lora.py
@@ -1,67 +1,78 @@
 import os
 import shutil
-os.environ["CUDA_VISIBLE_DEVICES"] = "1,2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "4,5"
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
-from trl import clone_chat_template, setup_chat_format
+from trl import setup_chat_format
 from huggingface_hub import HfApi
-from utils import print_trainable_params
-
-# Base model (same one you used during training)
-base_model = "Behzadshomali/Teuken3.7B"
-
-# Your LoRA model (local folder or HF Hub repo)
-lora_model = "/raid/s3/opengptx/behzad_shomali/instruction_tuning/Teuken3.73T_IT_OpenMathInstruct-2/2025_08_26-09_59_53_4096/checkpoint-15823/"
-output_dir = "/raid/s3/opengptx/behzad_shomali/instruction_tuning/Teuken3.73T_IT_OpenMathInstruct-2/2025_08_26-09_59_53_4096/checkpoint-15823/hf_converted/"
-
-# Load tokenizer
-tokenizer = AutoTokenizer.from_pretrained(base_model)
-
-# Load base model
-model = AutoModelForCausalLM.from_pretrained(
-    base_model,
-    device_map="auto",
-    trust_remote_code=True
-)
-
-# model, tokenizer, _ = clone_chat_template(model, tokenizer, "Qwen/Qwen3-0.6B")
-model, tokenizer = setup_chat_format(model, tokenizer)
-
-# Load LoRA on top of base
-model = PeftModel.from_pretrained(model, lora_model)
-print_trainable_params(model)
-
-# Merge LoRA adapters into the base model
-model = model.merge_and_unload()
-
-save_path = os.path.join(output_dir)
-model.save_pretrained(save_path, safe_serialization=True)
-tokenizer.save_pretrained(save_path)
-
-# Optionally copy your custom modeling file
-custom_modeling_src = "/home/behzad_shomali/modalities/src/modalities/instruction_finetuning/modeling_gpt2.py"
-custom_modeling_dst = os.path.join(save_path, "modeling_gpt2.py")
-shutil.copy(custom_modeling_src, custom_modeling_dst)
 
 
-# revision = "2025_08_26-09_59_53-checkpoint-5274"
-# model.push_to_hub(
-#     "Behzadshomali/Teuken3.7B_IT_LoRA-OpenMathInstruct-2",
-#     revision=revision,
-#     private=True,             # optional
-#     use_temp_dir=True,        # makes sure full files are synced
-#     commit_message="Add merged LoRA model with custom code",
-#     safe_serialization=True
-# )
 
-# tokenizer.push_to_hub("Behzadshomali/Teuken3.7B_IT_LoRA-OpenMathInstruct-2", revision=revision)
+def merge_lora_adapter(
+        lora_model, 
+        base_model, 
+        save_on_disk=True, 
+        upload_to_HF=False,
+        **kwargs_HF
+    ):
+    tokenizer = AutoTokenizer.from_pretrained(base_model)
+    model = AutoModelForCausalLM.from_pretrained(
+        base_model,
+        device_map="auto",
+        trust_remote_code=True
+    )
 
-# api = HfApi()
-# api.upload_file(
-#     path_or_fileobj="/home/behzad_shomali/modalities/src/modalities/instruction_finetuning/modeling_gpt2.py",       
-#     repo_id="Behzadshomali/Teuken3.7B_IT_LoRA-OpenMathInstruct-2",  
-#     path_in_repo="modeling_gpt2.py",     
-#     repo_type="model",
-#     revision=revision
-# )
+    if tokenizer.chat_template is None:
+        model, tokenizer = setup_chat_format(model, tokenizer)
+
+    # Load LoRA on top of base
+    model = PeftModel.from_pretrained(model, lora_model)
+
+    # Merge LoRA adapters into the base model
+    model = model.merge_and_unload()
+
+    if save_on_disk:
+        save_path = os.path.join(lora_model, "lora_merged")
+        model.save_pretrained(save_path, safe_serialization=True)
+        tokenizer.save_pretrained(save_path)
+
+        # the modeling_gpt2.py file is missing after merge,
+        # therefore it is copied manually
+        custom_modeling_src = "/home/behzad_shomali/modalities/src/modalities/instruction_finetuning/modeling_gpt2.py"
+        custom_modeling_dst = os.path.join(save_path, "modeling_gpt2.py")
+        shutil.copy(custom_modeling_src, custom_modeling_dst)
+
+    if upload_to_HF:
+        revision = kwargs_HF["revision"]
+        repo_id = kwargs_HF["repo_id"]
+        commit_message = kwargs_HF.get("commit_message", "Add merged LoRA model with custom code")
+
+        model.push_to_hub(
+            repo_id,
+            revision=revision,
+            private=True,
+            use_temp_dir=True, # makes sure full files are synced
+            commit_message=commit_message,
+            safe_serialization=True
+        )
+        tokenizer.push_to_hub(repo_id, revision=revision)
+
+        api = HfApi()
+        api.upload_file(
+            path_or_fileobj="/home/behzad_shomali/modalities/src/modalities/instruction_finetuning/modeling_gpt2.py",       
+            repo_id=repo_id,  
+            path_in_repo="modeling_gpt2.py",     
+            repo_type="model",
+            revision=revision
+        )
+
+if __name__ == "__main__":
+    base_model =  "Qwen/Qwen3-4B-Base" #"Behzadshomali/Teuken3.7B"
+    lora_model = "/raid/s3/opengptx/behzad_shomali/instruction_tuning/Teuken3.73T_IT_OpenMathInstruct-2/2025_08_30-19_33_56/rank8/checkpoint-9084/"
+    merge_lora_adapter(
+        lora_model, 
+        base_model,
+        save_on_disk=True,
+        upload_to_HF=False
+    )
