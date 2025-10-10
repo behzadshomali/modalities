8c729ddd6d74f0adc416e1e1a8df5ab4c5958a85
Behzad Shomali <behzad.shomali@gmail.com>
2025-10-10 14:54:30 +0200

    add a new script

---
+++ scripts/multilingual_proj/convert_pondering_to_HF.sh
@@ -0,0 +1,4 @@
+python -m modalities.conversion.pondering.convert_pondering_model \
+    /raid/s3/opengptx/behzad_shomali/modalities/config_files/hf_conversion/fineweb2_edu_pondering3_pythia_HF_conversion.yaml \
+    /raid/s3/opengptx/behzad_shomali/hf_converted/fineweb2_edu_pondering_pythia/ \
+    --num_testruns 3
