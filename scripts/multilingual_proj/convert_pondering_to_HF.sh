cc02e207ec0f3bb5059cc8803bb7e4833a0528d7
Behzad Shomali <behzad.shomali@gmail.com>
2025-10-13 14:24:43 +0200

    change folder naming structure

--- scripts/multilingual_proj/convert_pondering_to_HF.sh
+++ scripts/multilingual_proj/convert_pondering_to_HF.sh
@@ -1,4 +1,4 @@
 python -m modalities.conversion.pondering.convert_pondering_model \
     /raid/s3/opengptx/behzad_shomali/modalities/config_files/hf_conversion/fineweb2_edu_pondering3_pythia_HF_conversion.yaml \
-    /raid/s3/opengptx/behzad_shomali/hf_converted/fineweb2_edu_pondering_pythia/ \
-    --num_testruns 3
+    /raid/s3/opengptx/behzad_shomali/hf_converted/fineweb2_edu_pondering3_pythia/2025-10-07__14-48-42_64ab0eb2-88000/ \
+    --num_testruns 5
